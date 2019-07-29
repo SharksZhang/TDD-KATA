de2ac336d7cfc9f566f7708d8017071190d73ecc
ericzhang08 <yuzhangfish@163.com>
2019-07-29 10:29:54 -0400

    add simple encryption alternating split

--- codewars/src/simpleencryptionalternatingsplit/Kata.java
+++ codewars/src/simpleencryptionalternatingsplit/Kata.java
@@ -5,39 +5,57 @@ import java.util.function.Predicate;
 public class Kata {
 
     public static String encrypt(final String text, final int n) {
-        // Your code here
-//
-
         if (n <= 0) {
             return text;
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(getString(text, (i)->(i %2 != 0)));
 
-
-        for (int i = 0; i < text.length(); i++) {
-            if (i % 2 == 0) {
-                sb.append(text.charAt(i));
-            }
+        String result = text;
+        for (int i = 0; i < n; i++) {
+            result = singleEncrypt(result);
         }
-
-        return sb.toString();
+        return result;
     }
 
-    private static StringBuilder getString(String text, Predicate<Integer> p) {
+    private static String singleEncrypt(String text) {
+        return filter(text, (i) -> (i % 2 != 0)) + filter(text, (i) -> (i % 2 == 0));
+    }
+
+    private static String filter(String text, Predicate<Integer> p) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < text.length(); i++) {
             if (p.test(i)) {
                 sb.append(text.charAt(i));
             }
         }
-        return sb;
+        return sb.toString();
     }
 
 
     public static String decrypt(final String encryptedText, final int n) {
-        // Your code here
-        return null;
+        if (n <= 0) {
+            return encryptedText;
+        }
+
+        String result = encryptedText;
+        for (int i = 0; i < n; i++) {
+            result = singleDecrypt(result);
+        }
+        return result;
     }
 
+    private static String singleDecrypt(String encryptedText) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int j = encryptedText.length() / 2;
+
+        while (i < encryptedText.length() / 2 && j < encryptedText.length()) {
+            sb.append(encryptedText.charAt(j++));
+            sb.append(encryptedText.charAt(i++));
+        }
+
+        while (j < encryptedText.length()) {
+            sb.append(encryptedText.charAt(j++));
+        }
+        return sb.toString();
+    }
 }
